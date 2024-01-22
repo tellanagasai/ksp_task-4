a051526157d4b2020951654fffee2294744df419
TELLA NAGASAI <127427289+tellanagasai@users.noreply.github.com>
2024-01-22 21:07:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("helloworld");
+//Assigment op
+let number1 = 5
+number1 = number1 + 5;
+console.log(number1);
+//comapison op
+let number1 = 7;
+let number2 = 5;
+console.log(number1==7);
+console.log(number1===number2);
+//logical op
+let number3 = 9;
+let number4 = 8;
+console.log(number3 <= 9 && numbe4 <=8);
+//bitwise AND, OR , NOT and XOR
+let number5 = 5;
+let number9 = 3;
+console.log(number5 >=5 || number9 <=4);
+console.log(!true); //NOT operator returns the opposite of what is infront of it
+console.log(number5 & number9) //AND Operator
+console.log(number5 | number9) //OR Operator
+
